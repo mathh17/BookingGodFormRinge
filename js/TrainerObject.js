f8eab626215838fc17d19f9c8838cc4b51995714
Unknown <mathh17@student.sdu.dk>
2018-04-27 12:10:07 +0200

    objects ready

--- js/TrainerObject.js
+++ js/TrainerObject.js
@@ -1,5 +1,23 @@
 function TrainerObject(trainerName, trainerPicture, trainerDescription ){
     this.trainerName = trainerName;
-    this.trainerPicture = trainerPicture;
+    this.trainerPicture = trainerPicture.src;
     this.trainerDescription = trainerDescription;
 }
+        
+        var sørenPicture = new Image();
+        sørenPicture.src = "../Udklip5.png";
+        
+        var søren = new TrainerObject(
+            "Søren", 
+             sørenPicture, 
+            "Han hedder søren"
+        );
+
+        var katrinePicture = new Image();
+        katrinePicture.src = "../Udklip5.png";
+        
+        var katrine = new TrainerObject(
+            "Katrine", 
+             katrinePicture, 
+            "Hun hedder Katrine"
+        );
